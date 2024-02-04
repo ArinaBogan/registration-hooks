4c6d10eec3009d20475fb268deec0826f66f2293
ArinaBogan <arina.bogan@gmail.com>
2024-02-04 18:32:35 +0300

    added registration_hooks

--- registration_hooks/src/pages/LoginPage/LoginPage.js
+++ registration_hooks/src/pages/LoginPage/LoginPage.js
@@ -7,31 +7,29 @@ function LoginPage() {
     const [inp, setInp] = useState({ email: '', password: '' });
 
     const changeStateInp = (e) => {
-
         setInp({ ...inp, [e.target.name]: e.target.value })
     }
     const showInfo = () => {
         try {
             if (!inp.email || !inp.password) throw new Error('введите значение');
-            if (!/^[\w\d]+@[\w\d\.]+$/gm.test(inp.email)) throw new Error('некорректно введена почта');
+            if (!/^[\w\d\.\]+@[\w]+\.[\w]{2,4}$/gm.test(inp.email)) throw new Error('некорректно введена почта');
             console.log(inp);
-            setInp({ email: '', password: '' })
+            setInp({ email: '', password: '' });
         } catch (error) {
             console.log(error.message);
         }
     }
     return (
         <>
-            <h1>Registration</h1>
-            <TextField name='email' onChange={changeStateInp} id="outlined-basic" label="Email" variant="outlined" value={inp.email} />
-            <TextField name='password' onChange={changeStateInp} id="outlined-basic" label="Password" variant="outlined" value={inp.password} />
+            <h1>Login</h1>
+            <div>
+                <TextField name='email' onChange={changeStateInp} id="outlined-basic" label="Email" variant="outlined" value={inp.email} />
+            </div>
+            <div>
+                <TextField name='password' onChange={changeStateInp} id="outlined-basic" label="Password" variant="outlined" value={inp.password} />
+            </div>
             <Button onClick={showInfo} variant="outlined">Login</Button>
-            <p>
-                Don't have an account yet?
-                <Link to="/reg">
-                    <Button variant="text">SIGN UP</Button>
-                </Link>
-            </p>
+            <p>Don't have an account yet?<Link to="/reg"> <Button variant="text">SIGN UP</Button></Link></p>
 
         </>
     );
--- registration_hooks/src/pages/RegPage/RegPage.js
+++ registration_hooks/src/pages/RegPage/RegPage.js
@@ -2,7 +2,6 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import LoginPage from '../LoginPage/LoginPage';
 
 function RegPage() {
     const [inp, setInp] = useState({ email: '', password: '', confirm: '' });
@@ -14,7 +13,7 @@ function RegPage() {
     const showInfo = () => {
         try {
             if (!inp.email || !inp.password || !inp.confirm) throw new Error('введите значение');
-            if (!/^[\w\d]+@[\w\d\.]+$/gm.test(inp.email)) throw new Error('некорректно введена почта');
+            if (!/^[\w\d\.\]+@[\w]+\.[\w]{2,4}$/gm.test(inp.email)) throw new Error('некорректно введена почта');
             if (inp.password != inp.confirm) throw new Error('неверно введен пароль');
             console.log(inp);
             setInp({ email: '', password: '', confirm: '' })
@@ -25,16 +24,17 @@ function RegPage() {
     return (<>
         <>
             <h1>Registration</h1>
-            <TextField name='email' onChange={changeStateInp} id="outlined-basic" label="Email" variant="outlined" value={inp.email} />
-            <TextField name='password' onChange={changeStateInp} id="outlined-basic" label="Password" variant="outlined" value={inp.password} />
-            <TextField name='confirmPassword' onChange={changeStateInp} id="outlined-basic" label="ConfirmPassword" variant="outlined" value={inp.confirm} />
+            <div>
+                <TextField name='email' onChange={changeStateInp} id="outlined-basic" label="Email" variant="outlined" value={inp.email} />
+            </div>
+            <div>
+                <TextField name='password' onChange={changeStateInp} id="outlined-basic" label="Password" variant="outlined" value={inp.password} />
+            </div>
+            <div>
+                <TextField name='confirm' onChange={changeStateInp} id="outlined-basic" label="Confirm" variant="outlined" value={inp.confirm} />
+            </div>
             <Button onClick={showInfo} variant="outlined">Sign Up</Button>
-            <p>
-                Already have an account
-                <Link to="/">
-                    <Button variant="text">SIGN IN</Button>
-                </Link>
-            </p>
+            <p> Already have an account? <Link to="/"><Button variant="text">SIGN IN</Button>  </Link></p>
         </>
     </>);
 }
